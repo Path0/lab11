f3d6b14ab73da2defcdd562ac03f8362aa405d6d
pathopickax <62456068+Path0@users.noreply.github.com>
2022-04-13 17:36:33 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Lab-11-1/lb11.c
@@ -0,0 +1,54 @@
+/*****+-**--------*--*-*-**---*---*--***-*-**-******************************
+*
+*  Lab #: 11
+*
+*  Academic Integrity Statement:
+*
+*  I have not used source code obtained from any other unauthorized source, 
+*  either modified or unmodified.  Neither have I provided access to my code 
+*  to another. The effort I am submitting is my own original work.
+*
+*  Program Description:
+*
+******+-**--------*--*-*-**---*---*--***-*-**-*****************************/
+
+/*****+--**---*-*----*-****----------*-*-*-**-******************************
+*
+*  Function Information
+*
+*  Name of Function: 
+*
+*  Function Return Type: 
+*
+*  Parameters (list data type, name, and comment one per line):
+*  1.
+*  2. 
+*  3. 
+*
+*  Function Description: 
+*
+******+--**---*-*----*-****----------*-*-*-**-*****************************/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  
+  return 0;
+}
+
+/*****+--**---*-*----*-****----------*-*-*-**-******************************
+*
+*  Function Information
+*
+*  Name of Function: input
+*
+*  Function Return Type: int
+*
+*  Parameters (list data type, name, and comment one per line):
+*
+*  Function Description: Gets the seed input from the user that will be used 
+*  to calculate random values.
+*
+******+--**---*-*----*-****----------*-*-*-**-*****************************/
---
+++ .ccls-cache/@home@runner@Lab-11-1/main.c
@@ -0,0 +1,21 @@
+/*****+-**--------*--*-*-**---*---*--***-*-**-******************************
+*
+*  Lab #:12
+*
+*  Academic Integrity Statement:
+*
+*  I have not used source code obtained from any other unauthorized source, 
+*  either modified or unmodified.  Neither have I provided access to my code 
+*  to another. The effort I am submitting is my own original work.
+*
+*  Program Description:
+*
+******+-**--------*--*-*-**---*---*--***-*-**-*****************************/
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+printf("Hello World");
+return 0;
+}
